3566823bcae6c2509e48a8aae20e550e30f0b4b9
Nurbek <unurbek499@gmail.com>
2024-06-09 21:55:31 -0500

    second

--- apps/mainpage/urls.py
+++
@@ -1,7 +0,0 @@
-# apps/mainpage/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
--- apps/users/urls.py
+++
@@ -1,7 +0,0 @@
-# apps/users/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.register, name='register'),
-]
